8e1254b6b55a5e8dfef54be128dee9f8521e89ff
Emily Daykin <emilydaykin31@gmail.com>
2022-02-23 21:04:47 +0100

    Page refactor: changed index.html to load start page, and game.html to be the main game html. Added start.css and start.js for the load start page

---
+++ scripts/start.js
@@ -0,0 +1,27 @@
+const loadText = document.querySelector('.loading-text');
+const background = document.querySelector('.background');
+const button = document.querySelector('a');
+
+let load = 0;
+
+function blurring() {
+  load++;
+
+  if (load > 100) {
+    clearInterval(int);
+    // loadText.classList.add('hide');
+    loadText.style.display = 'none';
+    button.classList.remove('hide');
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+let int = setInterval(blurring, 20);
+
+// from Stack Overflow
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
