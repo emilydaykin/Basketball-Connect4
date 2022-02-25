52b0d04af1d67678a5ffada620880552b685a21f
Emily Daykin <emilydaykin31@gmail.com>
2022-02-25 13:10:47 +0100

    cleaned up game.js

--- scripts/components/game.js
+++ scripts/components/game.js
@@ -89,19 +89,19 @@ class Game {
     arrayOfCells.forEach((cell) => {
       cell.removeEventListener('mouseover', this.ballAppearOnTop);
       cell.removeEventListener('mouseleave', this.ballDisappearOnTop);
-    });
+    })
   }
 
   addClickEventListener(arrayOfCells) {
     arrayOfCells.forEach((cell) => {
       cell.addEventListener('click', this.verifyPlaceCheck);
-    });
+    })
   }
 
   removeClickEventListener(arrayOfCells) {
     arrayOfCells.forEach((cell) => {
       cell.removeEventListener('click', this.verifyPlaceCheck);
-    });
+    })
   }
   
   playGame(cells) {
@@ -116,52 +116,42 @@ class Game {
   computerToMoveNext() {
     let colNum;
     if (turn === 'computer') {
-      // Randomised choice
-      // colNum = Math.floor(Math.random() * 8);
-      // Smart choice:
-      // -- to be implemented later --
-      //
       // scan whole grid for orange cells
       if (document.querySelectorAll('.filled').length >= 5) {
         const orangeElements = document.querySelectorAll('#orange');
         const orangeCells = [...orangeElements].map((element) => element.dataset.id);
-        console.log('orange cells:', orangeCells);
         // BLOCK VERTICAL WINS:
         // loop through every col: if 3-in-a-row vertical:
         let entireColCells;
         let filteredOrangeColCells;
-        for (let i = 0; i < 8; i++) { 
+        for (let i = 0; i < gridWidth; i++) { 
           entireColCells = gridOverview.columns[`column${i}`].sort((a, b) => b - a);
           filteredOrangeColCells = entireColCells
             .filter((colCell) => orangeCells.includes(String(colCell)))
             .sort((a, b) => a - b);
-          // console.log(i, filteredOrangeColCells);
           let last = filteredOrangeColCells[filteredOrangeColCells.length-1];
           let first = filteredOrangeColCells[0];
           // check is cell directly above 3-in-a-row is filled
-          let cellAboveThree = document.querySelector(`div[data-id='${first-8}']`)
+          let cellAboveThree = document.querySelector(`div[data-id='${first-8}']`);
           // if 3-in-a-row oranges && cell above top one is in grid && empty:
-          if (filteredOrangeColCells.length > 2 && last - first === 16 && first - 8 > 7 && !cellAboveThree.classList.contains('filled')) {
-            // console.log(`col ${i} has a 3-in-a-row that can be blocked`)
+          if (filteredOrangeColCells.length > 2 && last - first === 16 && first - 8 > 7 
+            && !cellAboveThree.classList.contains('filled')) {
             colNum = i;
           }
         }
-        // BLOCK HORIZONTAL WINS:
+        // BLOCK some HORIZONTAL WINS:
         if (!colNum) {
           let entireRowCells;
           let filteredOrangeRowCells;
-          for (let i = 0; i < 5; i++) { 
+          for (let i = 0; i < gridHeight; i++) { 
             entireRowCells = gridOverview.rows[`row${i}`].sort((a, b) => b - a);
             filteredOrangeRowCells = entireRowCells
               .filter((rowCell) => orangeCells.includes(String(rowCell)))
               .sort((a, b) => a - b);
-            console.log(filteredOrangeRowCells);
-  
             let differences = [];
             for (let i = 0; i < filteredOrangeRowCells.length - 1; i++) {
               differences.push(filteredOrangeRowCells[i + 1] - filteredOrangeRowCells[i]);
             }
-            console.log('differences', differences);
             // Match consecutive 1s precisely 2 or more times:
             const regex = /1{2,}/g;
             if (differences.join('').match(regex) >= 1) {
@@ -176,41 +166,28 @@ class Game {
                   consecutiveCells = filteredOrangeRowCells.slice(-3);
                 }
               }
-              console.log('consecutiveCells', consecutiveCells);
               if (consecutiveCells) {
                 let first = consecutiveCells[0];
                 let last = consecutiveCells[2];
-                console.log('first', first);
-                console.log('last', last);
                 // check if directly left is a free cell to block:
                 let leftCell = document.querySelector(`div[data-id='${first - 1}']`);
                 let rightCell = document.querySelector(`div[data-id='${last + 1}']`);
-                console.log('leftCell', leftCell);
-                console.log('rightCell', rightCell);
                 if ((first - 1) % 8 <= 4 && !leftCell.classList.contains('filled')) {
                   colNum = (first - 1) % 8;
-                  console.log('column to fill:', colNum);
-                  console.log('---------horizontal 3 blocked to the left!')
                   // check if directly right is a free cell to block:
                 } else if ((last + 1) % 8 >= 3 && !rightCell.classList.contains('filled')) {
                   colNum = (last + 1) % 8;
-                  console.log('---------horizontal 3 blocked to the right!')
                 }
               }
             }
           }
         }
-        console.log('colNum to fill!!!!', colNum);
         if (!(colNum || colNum === 0)) {  // `!colNum` won't work since could be zero
           colNum = Math.floor(Math.random() * 8);
-          console.log('colNum (random) if no vertical or horizontal to block:', colNum);
         }
       } else {  // random move if board has <6 cells filled
         colNum = Math.floor(Math.random() * 8);
-        console.log('colNum if no grid < 6:', colNum);
       }
-      //
-      //
       let firstAvailableCell = this.verify(colNum);
       if (firstAvailableCell) {
         this.slideAndplace(colNum, firstAvailableCell);
@@ -220,7 +197,6 @@ class Game {
 
   verify(columnNumber) {
     // This modifies original gridOverview
-    console.log('COLUMN TO TRY:', columnNumber);
     const colContents = gridOverview.columns[`column${columnNumber}`].sort((a, b) => b - a);  
     // Get first available cell in that column:
     let firstAvailableCell = colContents.find((cell) => {
@@ -240,10 +216,7 @@ class Game {
       let colNum = event.target.getAttribute('data-id') % 8;
       let firstAvailableCell = this.verify(colNum);
       if (firstAvailableCell) {
-        // cells.forEach((cell) => cell.removeEventListener('click', this.verifyPlaceCheck));
         this.removeClickEventListener(cells);
-        // looseBallOrange.style.animation = 'shootingArc 1s linear';
-        // animation: shootingArc 1s linear infinite;
         // swishAudio.play();
         this.slideAndplace(colNum, firstAvailableCell);
         this.computerToMoveNext();
@@ -252,41 +225,34 @@ class Game {
   }
 
   slideBall = (columnNumber, cellToFill, yPositionEnd, looseBall, firstAvailableCell) => {
-
     document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-orange`);
     document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-blue`);
 
     this.removeHoverEventListener(cells);
-
     this.yPositionStart += 4;
     
     looseBall.style.top = `${this.yPositionStart + 1}px`;
 
-    // 50 because ~0.5*cellHeight, so that the 'animation' is smooth at the end
     if (this.yPositionStart > yPositionEnd) {
       clearInterval(this.slide);
       cellToFill.classList.add('filled');
       cellToFill.setAttribute('id', ballColour); 
       looseBall.classList.add('hide');
-  
       this.checkForAWin(columnNumber, firstAvailableCell);
       this.checkGameStatus();
       this.computerToMoveNext();
-
       this.addHoverEventListener(cells);
     }
   }
 
   slideAndplace(columnNumber, firstAvailableCell) {
     // remove this here again when it's the computer's turn:
-    // cells.forEach((cell) => cell.removeEventListener('click', this.verifyPlaceCheck));
     this.removeClickEventListener(cells);
     let cellToFill = document.querySelector(`div[data-id='${firstAvailableCell}']`);
-
+    
     // Get position of ball-start (top hidden row cell)
     let startingCell = document.querySelector(`div[data-id='${columnNumber}']`)
     let xPositionStart = startingCell.getBoundingClientRect().left  // same as end
-    // let xPositionStart = startingCell.getBoundingClientRect().left  
     this.yPositionStart = startingCell.getBoundingClientRect().top
     let yPositionEnd = cellToFill.getBoundingClientRect().top
 
@@ -300,14 +266,7 @@ class Game {
     this.yE = yPositionEnd;
     this.lB = looseBall;
     this.fAC = firstAvailableCell;
-
-    // setTimeout(thingToDo, delay)
-    // setInterval(thingToRepeatedlyDo, everySomethingSeconds)
-
-    this.slide = setInterval(() => this.slideBall(this.cN, this.cTF, this.yE, this.lB, this.fAC), 1)
-    // let setIntervalFunction = () => this.slide;
-    // setTimeout(() => setIntervalFunction(), 2000);
-    
+    this.slide = setInterval(() => this.slideBall(this.cN, this.cTF, this.yE, this.lB, this.fAC), 1)    
   }
 
   checkGameStatus() {
@@ -352,7 +311,6 @@ class Game {
     // Match consecutive 1s precisely 3 or more times:
     const regex = /1{3,}/g;
     if (differences.join('').match(regex) >= 1) {
-      // winnerAnnounced.innerText = `${ballColour} is the WINNER!!! (horizontal)`;
       this.getWinnerDeclaration()
       winner = ballColour;
       // get winning cells:
@@ -410,7 +368,6 @@ class Game {
         console.log('direction invalid');
     }
     if (winningCondition) {
-      // winnerAnnounced.innerText = `${ballColour} is the WINNER!!! (${direction})`;
       this.getWinnerDeclaration()
       winner = ballColour;
       theWinningCells = filteredCells.slice(0, 4);
@@ -423,65 +380,64 @@ class Game {
 
   checkForAWin(columnNumber, firstAvailableCell) {
 
-    const lastPlayedCell = firstAvailableCell;
-    const rowNum = Math.floor(lastPlayedCell / 8) - 1;
-    const diagUpNum = (lastPlayedCell - 11) % 7;
-    const diagDownNum = (lastPlayedCell - 7) % 9;
-    
-    const colCells = gridOverview.columns[`column${columnNumber}`];
-    const rowCells = gridOverview.rows[`row${rowNum}`];
-    let diagUpCells = ((diagUpNum >= 0 && diagUpNum < 6) 
-      ? gridOverview.diagonalUphill[`diagUp${diagUpNum}`] 
-      : null);
-    // safety check: since (16-11)%7=5, but should be null
-    // (this is not a problem for diagDown)
-    if (diagUpCells && !diagUpCells.includes(lastPlayedCell)) {
-      diagUpCells = null;
-    }
-    const diagDownCells = ((diagDownNum >= 0 && diagDownNum < 6) 
-      ? gridOverview.diagonalDownhill[`diagDown${diagDownNum}`] 
-      : null);
-    
-    // IF MORE THAN 6 CELLS FILLED IN WHOLE GRID:
-    // 4.0.4: Get all cell numbers that have ballColour ids:
-    const playerXElements = document.querySelectorAll(`#${ballColour}`);
-    // To map over nodelist: first convert to array:
-    const playerXCells = [...playerXElements].map((element) => element.dataset.id);
-    
-    // let winningCells;
-    this.winningCells = this.crossReferenceCells('horizontal', rowCells, playerXCells);
-    if (!this.winningCells) {
-      this.winningCells = this.crossReferenceCells('vertical', colCells, playerXCells);
-    }
-    if (!this.winningCells) {
-      this.winningCells = this.crossReferenceCells('diagUp', diagUpCells, playerXCells);
-    }
-    if (!this.winningCells) {
-      this.winningCells = this.crossReferenceCells('diagDown', diagDownCells, playerXCells);
-    }
+    if (document.querySelectorAll('.filled').length >= 5) {
+      const lastPlayedCell = firstAvailableCell;
+      const rowNum = Math.floor(lastPlayedCell / 8) - 1;
+      const diagUpNum = (lastPlayedCell - 11) % 7;
+      const diagDownNum = (lastPlayedCell - 7) % 9;
+      
+      const colCells = gridOverview.columns[`column${columnNumber}`];
+      const rowCells = gridOverview.rows[`row${rowNum}`];
+      let diagUpCells = ((diagUpNum >= 0 && diagUpNum < 6) 
+        ? gridOverview.diagonalUphill[`diagUp${diagUpNum}`] 
+        : null);
+      // safety check: since (16-11)%7=5, but should be null
+      // (this is not a problem for diagDown)
+      if (diagUpCells && !diagUpCells.includes(lastPlayedCell)) {
+        diagUpCells = null;
+      }
+      const diagDownCells = ((diagDownNum >= 0 && diagDownNum < 6) 
+        ? gridOverview.diagonalDownhill[`diagDown${diagDownNum}`] 
+        : null);
+      
+      // Get all cell numbers that have ballColour ids:
+      const playerXElements = document.querySelectorAll(`#${ballColour}`);
+      // To map over nodelist: first convert to array:
+      const playerXCells = [...playerXElements].map((element) => element.dataset.id);
+      this.winningCells = this.crossReferenceCells('horizontal', rowCells, playerXCells);
+      if (!this.winningCells) {
+        this.winningCells = this.crossReferenceCells('vertical', colCells, playerXCells);
+      }
+      if (!this.winningCells) {
+        this.winningCells = this.crossReferenceCells('diagUp', diagUpCells, playerXCells);
+      }
+      if (!this.winningCells) {
+        this.winningCells = this.crossReferenceCells('diagDown', diagDownCells, playerXCells);
+      }
 
-    // after clicked, make hovered ball (top row) disappear:
-    document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-orange`);
-    document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-blue`);
+      // after clicked, make hovered ball (top row) disappear:
+      document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-orange`);
+      document.querySelector(`div[data-id='${columnNumber}']`).classList.remove(`hovered-blue`);
+    }
 
     this.switchTurnOrEndGame();
   }
   
   getWinnerDeclaration() {
     if (modeSelected === '1player') {
-      winnerAnnounced.innerText = `${ballColour === 'orange' ? 'You are' : 'Your mysterious opponent is'} the WINNER!!!`;
+      winnerAnnounced.innerText = 
+        `${ballColour === 'orange' ? 'You are' : 'Your mysterious opponent is'} the WINNER!!!`;
     } else {
-      winnerAnnounced.innerText = `${ballColour === 'orange' ? 'Player 1' : 'Player 2'} is the WINNER!!!`;
+      winnerAnnounced.innerText = 
+        `${ballColour === 'orange' ? 'Player 1' : 'Player 2'} is the WINNER!!!`;
     }
   }
 
   switchTurnOrEndGame() {
     let totalCellsFilled = document.querySelectorAll('.filled').length;
-    // Other player turn: change ball colour;
     // if no winner and board not filled:
     if (!winner && totalCellsFilled < 40) {
       ballColour = ballColour === 'orange' ? 'blue' : 'orange';
-      // cells.forEach((cell) => cell.addEventListener('click', this.verifyPlaceCheck));
       this.addClickEventListener(cells)
       if (modeSelected === '1player') {
         turn = turn === 'player' ? 'computer' : 'player';
@@ -494,7 +450,6 @@ class Game {
         document.querySelector(`div[data-id='${winningCell}']`).classList.add('pulsate')
       })
     } else if (totalCellsFilled === 40) {
-      console.log("It's a draw!");
       winnerAnnounced.innerText = `It's a draw!`;
       gameStatus = 'ended';
     }
